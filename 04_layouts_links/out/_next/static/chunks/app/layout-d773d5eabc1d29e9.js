9a62e222d477caca0782288096517891c915c069
Nicholas Eruba <nicholasc1665@yahoo.com>
2024-03-08 13:35:01 +0000

    build all 1.0

---
+++ 04_layouts_links/out/_next/static/chunks/app/layout-d773d5eabc1d29e9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{526:function(e,n,t){Promise.resolve().then(t.t.bind(t,7095,23)),Promise.resolve().then(t.t.bind(t,1010,23)),Promise.resolve().then(t.t.bind(t,5992,23))},5992:function(){}},function(e){e.O(0,[854,253,698,744],function(){return e(e.s=526)}),_N_E=e.O()}]);
---
+++ 05_styles_fonts_images/README.md
@@ -0,0 +1,8 @@
+## 05_styles_fonts_images
+
+* using nextjs unique methods to easily set fonts
+    const rubik = Rubik({ subsets: ['latin'] })
+
+* set global styles in the global css file, set custom classes using tail wind
+
+* set primary color in the tailwind config file
